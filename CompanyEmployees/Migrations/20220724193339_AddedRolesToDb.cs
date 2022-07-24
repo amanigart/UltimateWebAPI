43330a465dedb483015a2f314219a22fc904cec9
amanigart <amanigart@outlook.com>
2022-07-24 22:16:38 +0200

    JWT Identity chapter

---
+++ CompanyEmployees/Migrations/20220724193339_AddedRolesToDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyEmployees.Migrations
+{
+    public partial class AddedRolesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "4f8083c0-3939-47ce-a81b-6f921f282c5c", "0f211072-6a66-431c-bea9-82b1b61ae3bc", "Administrator", "ADMINISTRATOR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "fcdee8fd-fb2b-4f26-9a0e-fea73d336812", "2c94d0dc-c934-4b4e-9110-dafa4d19291e", "Manager", "MANAGER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4f8083c0-3939-47ce-a81b-6f921f282c5c");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fcdee8fd-fb2b-4f26-9a0e-fea73d336812");
+        }
+    }
+}
